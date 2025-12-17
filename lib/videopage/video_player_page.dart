677d5a8158082be02ed655016478b6cc0700a147
rezaaplvv <rezapahlepi77654@gmail.com>
2025-12-17 11:05:31 +0700

    Menambahkan pemutar video fullscreen dengan auto rotasi layar

--- lib/videopage/video_player_page.dart
+++ lib/videopage/video_player_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:flutter/services.dart';
 
 class VideoPlayerPage extends StatefulWidget {
   final String videoUrl;
@@ -17,101 +18,52 @@ class VideoPlayerPage extends StatefulWidget {
 
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
   late VideoPlayerController _controller;
-  bool _isPlaying = false;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = VideoPlayerController.network(widget.videoUrl)
+    _controller = VideoPlayerController.networkUrl(
+      Uri.parse(widget.videoUrl),
+    )
       ..initialize().then((_) {
         setState(() {});
+        _controller.play();
       });
 
-    _controller.addListener(() {
-      setState(() {
-        _isPlaying = _controller.value.isPlaying;
-      });
-    });
+    // 🔒 Default portrait
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    super.dispose();
-  }
 
-  String _formatDuration(Duration duration) {
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return "$minutes:$seconds";
+    // 🔓 Balik ke normal saat keluar
+    SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.black,
-      ),
       body: Center(
         child: _controller.value.isInitialized
-            ? Column(
-                children: [
-                  AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  ),
-
-                  // PROGRESS BAR
-                  VideoProgressIndicator(
-                    _controller,
-                    allowScrubbing: true,
-                    colors: const VideoProgressColors(
-                      playedColor: Colors.blue,
-                      bufferedColor: Colors.grey,
-                      backgroundColor: Colors.white24,
-                    ),
-                  ),
-
-                  // TIME + PLAY BUTTON
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 8),
-                    child: Row(
-                      children: [
-                        Text(
-                          _formatDuration(_controller.value.position),
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                        const Spacer(),
-                        IconButton(
-                          iconSize: 40,
-                          color: Colors.white,
-                          icon: Icon(
-                            _isPlaying
-                                ? Icons.pause_circle
-                                : Icons.play_circle,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isPlaying
-                                  ? _controller.pause()
-                                  : _controller.play();
-                            });
-                          },
-                        ),
-                        const Spacer(),
-                        Text(
-                          _formatDuration(_controller.value.duration),
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+            ? GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller.value.isPlaying
+                        ? _controller.pause()
+                        : _controller.play();
+                  });
+                },
+                child: AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
               )
             : const CircularProgressIndicator(color: Colors.white),
       ),
